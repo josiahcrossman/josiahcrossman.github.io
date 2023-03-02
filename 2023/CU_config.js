7e4665c650a87dfe9504abaec089424627be5045
Josiah Crossman <55766072+josiahcrossman@users.noreply.github.com>
2023-03-01 21:38:03 -0800

    updating event

--- 2023/CU_config.js
+++ 2023/CU_config.js
@@ -7,7 +7,7 @@ var config_data = `
     { "name": "Event",
       "code": "e",
       "type": "event",
-      "defaultValue": "2022carv",
+      "defaultValue": "2023orore",
       "required": "true",
       "disabled": "true"
     },
